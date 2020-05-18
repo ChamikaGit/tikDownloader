2def0767d10b817c5b41572470ab522b8f480b32
Chamika Sehan Weerasekara <chamikashe@gmail.com>
2020-05-19 00:28:15 +0530

    new app without api for tiktok video downloading

---
+++ app/src/main/java/com/cd/statussaver/activity/SplashScreen.java
@@ -0,0 +1,103 @@
+package com.cd.statussaver.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cd.statussaver.BuildConfig;
+import com.cd.statussaver.R;
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.model.UpdateAvailability;
+import com.google.android.play.core.tasks.Task;
+
+import static com.google.android.play.core.install.model.AppUpdateType.IMMEDIATE;
+
+public class SplashScreen extends AppCompatActivity {
+    SplashScreen activity;
+    Context context;
+    AppUpdateManager appUpdateManager;
+    TextView tvVersionNO;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        tvVersionNO = findViewById(R.id.tvVersionNO);
+        context = activity = this;
+        appUpdateManager = AppUpdateManagerFactory.create(context);
+        UpdateApp();
+        tvVersionNO.setText("v"+ BuildConfig.VERSION_NAME);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        activity = this;
+
+        appUpdateManager.getAppUpdateInfo().addOnSuccessListener(appUpdateInfo -> {
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                try {
+                    appUpdateManager.startUpdateFlowForResult(
+                            appUpdateInfo, IMMEDIATE, activity, 101);
+                } catch (IntentSender.SendIntentException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void HomeScreen() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+            }
+        }, 2000);
+
+    }
+
+    public void UpdateApp() {
+        try {
+            Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
+            appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
+                if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                        && appUpdateInfo.isUpdateTypeAllowed(IMMEDIATE)) {
+                    try {
+                        appUpdateManager.startUpdateFlowForResult(
+                                appUpdateInfo, IMMEDIATE, activity, 101);
+                    } catch (IntentSender.SendIntentException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    HomeScreen();
+                }
+            }).addOnFailureListener(e -> {
+                e.printStackTrace();
+                HomeScreen();
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101) {
+            if (resultCode != RESULT_OK) {
+                HomeScreen();
+            } else {
+                HomeScreen();
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/cd/statussaver/util/AdsUtils.java
@@ -0,0 +1,83 @@
+package com.cd.statussaver.util;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+
+
+import com.cd.statussaver.R;
+import com.facebook.ads.Ad;
+import com.facebook.ads.AdError;
+import com.facebook.ads.AdSize;
+import com.facebook.ads.AdView;
+import com.facebook.ads.InterstitialAd;
+import com.facebook.ads.InterstitialAdListener;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import static com.facebook.ads.AudienceNetworkAds.TAG;
+
+public class AdsUtils {
+
+    public static void showGoogleBannerAd(Context context, com.google.android.gms.ads.AdView googleAdView) {
+
+        googleAdView.setVisibility(View.VISIBLE);
+        //Load Banner Ad
+        MobileAds.initialize(context, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        AdRequest adRequest = new AdRequest.Builder().build();
+        googleAdView.loadAd(adRequest);
+    }
+
+    public static void showGoogleInterstitialAd(Context context) {
+
+        MobileAds.initialize(context, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        final com.google.android.gms.ads.InterstitialAd mInterstitialAd = new com.google.android.gms.ads.InterstitialAd(context);
+        mInterstitialAd.setAdUnitId(context.getResources().getString(R.string.admob_interstitial_ad));
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                mInterstitialAd.show();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+            }
+
+            @Override
+            public void onAdOpened() {
+            }
+
+            @Override
+            public void onAdClicked() {
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+            }
+
+            @Override
+            public void onAdClosed() {
+            }
+        });
+
+
+    }
+
+
+
+
+}
